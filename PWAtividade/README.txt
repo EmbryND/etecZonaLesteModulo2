b1b18697ce7225e45c018939f366e1bed3456167
EmbryND <elvislimasamuel27@gmail.com>
2024-11-12 18:08:35 -0300

    PWA e manifest.

---
+++ PWAtividade/README.txt
@@ -0,0 +1,3 @@
+Para iniciar o arquivo, abra-o em um servidor local. 
+Após fazer isso, aperte a tecla F12 e vá na aba 'Applications'.
+E pronto, o manifest estará lá!!!
---
+++ PWAtividade/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'feiratec-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/images/icon-192x192.png',
+  '/images/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
