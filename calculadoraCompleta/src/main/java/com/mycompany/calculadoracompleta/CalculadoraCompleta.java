1fc31d60e43ba5c273489e4734bdb4ca5c67428a
EmbryND <elvislimasamuel27@gmail.com>
2024-12-01 15:45:57 -0300

    Calculadora em Java

---
+++ calculadoraCompleta/src/main/java/com/mycompany/calculadoracompleta/CalculadoraCompleta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoracompleta;
+
+/**
+ *
+ * @author Samuel
+ */
+public class CalculadoraCompleta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
